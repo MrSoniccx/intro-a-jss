fa3e9425fab76c1d2bbac43e6fe5d589150a8561
MrSoniccx <yahel_isordia@hotmail.com>
2019-09-17 19:08:00 -0500

    avance

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo!')
+var nombre = 'Juan Pérez'
+var edad = 22
+var email
+
+console.log(nombre)
+console.log(edad)
+console.log(email)
+
+var edad = 'veintidos'
+console.log(edad)
