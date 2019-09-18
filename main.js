1ab9c9926b660a7b39fd3a3e78bf4c93500d21e3
MrSoniccx <yahel_isordia@hotmail.com>
2019-09-17 19:34:40 -0500

    asdasdad

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var nombre = 'Juan Pérez'
 var edad = 22
 var email
+var esEstudiante = true
+var num1 = 5
+var num2 = 2
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+var resultado= num1+num2
+console.log(resultado)
+
+
+console.log( nombre + ' tiene' + edad + ' años. ¿Es estudiante? ' + esEstudiante)
 
 console.log(nombre)
 console.log(edad)
@@ -8,3 +22,7 @@ console.log(email)
 
 var edad = 'veintidos'
 console.log(edad)
+
+nombre = prompt('¿Como te llamas?')
+
+alert('Hola ' + nombre)
